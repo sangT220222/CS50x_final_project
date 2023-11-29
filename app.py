8385de59cdd2cf5e370d0d62fc57c44f6445b1bb
sangT220222 <magnas9@hotmail.co.uk>
2023-11-29 22:51:06 +0000

    login & register files, added register step in app

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 import os
 
+import sqlite3
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-import requests #double check this
+import requests
 
 app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+database = sqlite3.connect('final_project.db')
+db = database.cursor()
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -21,14 +26,16 @@ def login():
     session.clear()
 
     # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
+    if request.method == "POST": #here it will check the tag with "method = post" and goes on from there
 
         # Ensure username was submitted
         if not request.form.get("username"):
+        #here, the tag in /login path (login.html file in this case) is used to check if text has been input. The "name" element in the html tag is used to link here
             return "Username required"
 
         # Ensure password was submitted
         elif not request.form.get("password"):
+        #here, the tag in /login path (login.html file in this case) is used to check if text has been input. The "name" element in the html tag is used to link here
             return "Password required"
 
         # Query database for username
@@ -36,7 +43,7 @@ def login():
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password")
+            return "invalid username and/or password"
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -47,6 +54,46 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+
+        users_table = db.execute("SELECT username FROM users")
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return "must provide username"
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return "must provide password"
+
+        username = request.form.get("username")
+
+        for name in users_table:
+            if name['username'] == username:
+                return "username is taken, try a new username"
+
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if password == confirmation:
+
+            hashed_password = generate_password_hash(password) #hashing the password input by the user
+
+            db.execute("INSERT INTO users (username,hash) VALUES(?,?)", username, hashed_password)
+
+            db.close()
+            database.close()
+
+            # Redirect user to home page
+            return redirect("/")
+        else:
+            return "passwords entered do not match"
+
+    else:
+        return render_template("register.html")
 
 @app.route("/search_cuisine", methods = ["POST"])
 def search(): #we will update this, for now we are writing the blueprint
